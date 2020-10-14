4ed8e99e5d13a5934b378d69723d17f534f0d438
RemyBen <pntdu83@gmail.com>
2020-10-14 11:03:55 +0200

    add afficheUnEmploye

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Employe;
 
 class PrincipalController extends AbstractController
 {
@@ -29,5 +31,27 @@ class PrincipalController extends AbstractController
      */
     public function infos($dep, $sexe) {
         return $this->render('principal/infos.html.twig', array("dep" => $dep, "sexe" => $sexe));
+    }
+    
+    /**
+     * @Route("/employes", name="employes")
+     * @param RegistryInterface $doctrine
+     */
+    public function afficheEmployes(ManagerRegistry $doctrine) {
+        $employes = $doctrine->getRepository(Employe::class)->findAll();
+        $titre = "Liste des employés";
+        return $this->render('principal/employes.html.twig', compact('titre', 'employes'));
+    }
+    
+    /**
+     * @Route("/employe/{id}", name="unEmploye", requirements={"id":"\d+"})
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * @return type
+     */
+    public function afficheUnEmploye(ManagerRegistry $doctrine, int $id) {
+        $employe = $doctrine->getRepository(Employe::class)->find($id);
+        $titre = "Employé n° " . $id;
+        return $this->render('principal/unEmploye.html.twig', compact('titre', 'employe'));
     }
 }
